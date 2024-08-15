de4ae8511de07ae985f05885fe6e0d895a21b72d
wasabiQuoc <wasabi.together@gmail.com>
2024-08-15 14:08:34 +0700

    create lobbies

--- Assets/Scripts/Networking/Hosts/HostGameManager.cs
+++ Assets/Scripts/Networking/Hosts/HostGameManager.cs
@@ -9,11 +9,15 @@ using Unity.Netcode.Transports.UTP;
 using Unity.Netcode;
 using Unity.Networking.Transport.Relay;
 using UnityEngine.SceneManagement;
+using Unity.Services.Lobbies;
+using Unity.Services.Lobbies.Models;
 
 public class HostGameManager
 {
     private Allocation allocation;
     private string joinCode;
+    private string lobbyId;
+
     private const int MaxConnections = 20;
     private const string GameScene = "Game";
     public async Task StartHostAsync()
@@ -42,7 +46,38 @@ public class HostGameManager
         RelayServerData relayServerData = new RelayServerData(allocation, "dtls");
         unityTransport.SetRelayServerData(relayServerData);
 
+        try
+        {
+            CreateLobbyOptions lobbyOptions = new CreateLobbyOptions();
+            lobbyOptions.IsPrivate = false;
+            lobbyOptions.Data = new Dictionary<string, DataObject>()
+            {
+                {
+                    "JoinCode", new DataObject(
+                        visibility: DataObject.VisibilityOptions.Member,
+                        value: joinCode
+                        )
+                }
+            };
+            Lobby lobby = await Lobbies.Instance.CreateLobbyAsync("My lobby", MaxConnections, lobbyOptions);
+            lobbyId = lobby.Id;
+            HostSingleton.Instance.StartCoroutine(HeartBeatLobby(15));
+        }
+        catch(LobbyServiceException e)
+        {
+            Debug.LogError(e);
+            return;
+        }
+
         NetworkManager.Singleton.StartHost();
         NetworkManager.Singleton.SceneManager.LoadScene(GameScene, LoadSceneMode.Single);
+    }
+    private IEnumerator HeartBeatLobby(float waitTimeSecond)
+    {
+        while(true)
+        {
+            Lobbies.Instance.SendHeartbeatPingAsync(lobbyId);
+            yield return new WaitForSecondsRealtime(waitTimeSecond);
+        }
     }
 }
