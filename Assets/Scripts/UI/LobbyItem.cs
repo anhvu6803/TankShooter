d7a8338ed06d8e47c26a630c98bd75d5857eafc0
wasabiQuoc <wasabi.together@gmail.com>
2024-08-15 14:57:52 +0700

    joining lobbies

--- Assets/Scripts/UI/LobbyItem.cs
+++ Assets/Scripts/UI/LobbyItem.cs
@@ -1,18 +1,26 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
+using Unity.Services.Lobbies.Models;
 using UnityEngine;
 
 public class LobbyItem : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
-    // Update is called once per frame
-    void Update()
+    [SerializeField] private TMP_Text lobbyNameText;
+    [SerializeField] private TMP_Text lobbyPlayersText;
+    private LobbiesList lobbiesList;
+    private Lobby lobby;
+    public void Initialise(Lobby lobby, LobbiesList lobbiesList)
     {
-        
+        this.lobbiesList = lobbiesList;
+        this.lobby = lobby;
+
+        lobbyNameText.text = lobby.Name;
+        lobbyPlayersText.text = $"{lobby.Players.Count}/{lobby.MaxPlayers}";
+    }
+    public void Join()
+    {
+        lobbiesList.JoinAsync(lobby);
     }
 }
